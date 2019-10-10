67a4f73563964ac8a13a0bee6e46b94297301a3a
Fadila07 <rfadila195@gmail.com>
2019-10-10 08:45:14 +0700

    perubahan

---
+++ app/src/main/java/com/example/laundryargan/tampilan/Datatransaksi.java
@@ -0,0 +1,16 @@
+package com.example.laundryargan.tampilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.laundryargan.R;
+
+public class Datatransaksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datatransaksi);
+    }
+}
